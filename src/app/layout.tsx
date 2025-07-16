43b9030a5cb78990b54cd36210861775f91127e5
DangThanhWang <125245112+DangThanhWang@users.noreply.github.com>
2025-07-17 02:50:19 +0700

    Add Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Providers from "@/components/Providers";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Providers>
           {children}
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
